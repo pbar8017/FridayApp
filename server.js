c8d6954cc5c1dd578f9dc91c2995e3139c423898
pbar8017 <pbar8017@gmail.com>
2025-01-26 13:19:27 +1100

    server.

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/login', async (req, res) => {
 });
 
 // Start server
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
